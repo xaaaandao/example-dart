449b211a9840bd038b4062fb555e5a4155065cdc
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-03-07 17:30:31 -0300

    Add arquivos

---
+++ tipos-variaveis.dart
@@ -0,0 +1,10 @@
+void main(){
+    // Tipo variável genérico
+    var nome = "Ronaldo";
+
+    String email = "teste@gmail.com"
+
+    int numero = 50;
+    double preco = 19.99;
+    bool booleano = true;
+}
---
+++ variaveis-constantes-print.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Declaração de variável
+  var nome = "Ronaldo";
+  print(nome);
+  
+  // Constantes
+  const pi = 3.14;
+  print(pi);
+  
+  // Comentários várias linhas
+  /*
+   * 
+   * */
+  
+  var salario = 100;
+  var bonus = 20;
+  var total = salario + bonus;
+  print("O salário total é: $total");
+}
